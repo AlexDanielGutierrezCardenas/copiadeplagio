85674c5d4ea32c9f709b506f7ddcd5f8fdcf39a6
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2022-07-22 19:20:49 -0400

    aumentando carpeta utils

--- src/context/Register/Register.js
+++ src/context/Register/Register.js
@@ -4,21 +4,11 @@ import { Grid, Paper, Avatar, Typography} from '@material-ui/core';
 import useForm from '../../hooks/useForm';
 // import {headleSubmit,onInputChange} from '../Register/Utils/utils';
 import './Register.css';
+import { avatarStyle, headerStyle, paperStyle, headerStyle2, headerStyle3, headerStyle4, headerStyle5, headerStyle6, headerStyle7, headerStyle8, headerStyle9, headerStyle10} from './utils/estilo';
 
 
 export const Register = () => {
-const paperStyle={padding:'10px 30px',width:'540px', height:'631px' ,margin:'30px auto',radius:'8px'}
-const headerStyle={width:'494px',height:'24px',top:'100px',left:'32px',opacity:'70%',fontWeight:'bold',fontSize:'19px',fontFamily:'Mulish',Weigth:'700',size:'19px',lineHeight:'23.85px',letter:'0.4px',align:'center'}
-const headerStyle2={width:'494px',height:'30px',top:'156px',left:'32px',fontWeight:'bold',fontSize:'24px',fontFamily:'Mulish',Weigth:'700',size:'24px',lineHeight:'30.12px',letter:'0.3px',align:'center'}
-const headerStyle3={width:'494px',height:'20px',top:'198px',left:'32px',fontWeight:'regular',fontSize:'14px',fontFamily:'Mulish',Weigth:'400',size:'14px',lineHeight:'20px',letter:'0.3px',align:'center'}
-const headerStyle4={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
-const headerStyle5={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
-const headerStyle6={width:'85px',height:'13px',top:'354px',left:'441px',fontWeight:'regular',fontFamily:'Mulish',Weigth:'400',size:'10px',lineHeight:'12.55px',letter:'0.1px',align:'right'}
-const headerStyle7={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
-const headerStyle8={width:'149px',height:'20px',top:'571px',left:'177px',fontWeight:'regular',fontFamily:'Mulish',Weigth:'400',size:'14px',lineHeight:'20px',letter:'0.3px',align:'center'}
-const headerStyle9={width:'51px',height:'20px',fontWeight:'semibold',fontFamily:'Mulish',Weigth:'600',size:'14px',lineHeight:'20px',letter:'0.2px',align:'center'}
-const headerStyle10={width:'446px',height:'20px',top:'15px',left:'24px',fontWeight:'semibold',fontFamily:'Mulish',Weigth:'600',size:'14px',lineHeight:'20px',letter:'0.2px',align:'center',color:'#FFFFFF'}
-const avatarStyle={width:'48px', height:'48px',background:'blue'}
+
 const [input,setInput, error,setError] = useForm();
     
 const onInputChange = e => {
---
+++ src/context/Register/utils/estilo.js
@@ -0,0 +1,12 @@
+export const paperStyle={padding:'10px 30px',width:'540px', height:'631px' ,margin:'30px auto',radius:'8px'}
+export const headerStyle={width:'494px',height:'24px',top:'100px',left:'32px',opacity:'70%',fontWeight:'bold',fontSize:'19px',fontFamily:'Mulish',Weigth:'700',size:'19px',lineHeight:'23.85px',letter:'0.4px',align:'center'}
+export const headerStyle2={width:'494px',height:'30px',top:'156px',left:'32px',fontWeight:'bold',fontSize:'24px',fontFamily:'Mulish',Weigth:'700',size:'24px',lineHeight:'30.12px',letter:'0.3px',align:'center'}
+export const headerStyle3={width:'494px',height:'20px',top:'198px',left:'32px',fontWeight:'regular',fontSize:'14px',fontFamily:'Mulish',Weigth:'400',size:'14px',lineHeight:'20px',letter:'0.3px',align:'center'}
+export const headerStyle4={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
+export const headerStyle5={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
+export const headerStyle6={width:'85px',height:'13px',top:'354px',left:'441px',fontWeight:'regular',fontFamily:'Mulish',Weigth:'400',size:'10px',lineHeight:'12.55px',letter:'0.1px',align:'right'}
+export const headerStyle7={width:'494px',height:'16px',fontWeight:'bold',fontSize:'12px',fontFamily:'Mulish',Weigth:'700',size:'12px',lineHeight:'15.06px',letter:'0.3px'}
+export const headerStyle8={width:'149px',height:'20px',top:'571px',left:'177px',fontWeight:'regular',fontFamily:'Mulish',Weigth:'400',size:'14px',lineHeight:'20px',letter:'0.3px',align:'center'}
+export const headerStyle9={width:'51px',height:'20px',fontWeight:'semibold',fontFamily:'Mulish',Weigth:'600',size:'14px',lineHeight:'20px',letter:'0.2px',align:'center'}
+export const headerStyle10={width:'446px',height:'20px',top:'15px',left:'24px',fontWeight:'semibold',fontFamily:'Mulish',Weigth:'600',size:'14px',lineHeight:'20px',letter:'0.2px',align:'center',color:'#FFFFFF'}
+export const avatarStyle={width:'48px', height:'48px',background:'blue'}
